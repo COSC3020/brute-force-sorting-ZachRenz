0af41e382eec53351c7f8c78c314a9b2374d12bd
ZachRenz <zachary.ginger539@gmail.com>
2024-04-02 00:02:19 -0600

    finished

--- code.js
+++ code.js
@@ -1,3 +1,36 @@
 function permutationSort(a) {
-    return 0;
+    
+    let count = 0;
+    let sorted = false;
+    function permute(a, i){
+        if (i == a.length - 1){
+            count++;
+            if(checkSort(a)){
+                sorted = true;
+                return count;
+            }
+        }
+        for(let j = i; j < a.length; j++){
+            [a[i], a[j]] = [a[j], a[i]];
+            permute(a, i+1);
+            if(sorted == true){
+                return count;
+            }
+            [a[j], a[i]] = [a[i], a[j]];
+        }
+    }
+    permute(a, 0);
+    
+    return count;
 }
+
+function checkSort(a){
+    let sort = true;
+    for(let i = 0; i < a.length; i++){
+        if(a[i] > a[i+1]){
+            sort = false;
+        }
+    }
+    return sort;
+}
+
