9f1a30540275d489f8492e7cca701515a4c4f03a
Zachary Renz <157152808+ZachRenz@users.noreply.github.com>
2024-05-07 13:25:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,9 @@ function permutationSort(a) {
     
     let count = 0;
     let sorted = false;
-    function permutations(a, i){
+    // I did look at MelkMan419's implementation to find out how to traverse a permutation tree
+    // recursively, but now my understanding of this is complete. 
+    function permutations(a, i){ 
         if (i == a.length - 1){
             count++;
             if(checkSort(a)){
